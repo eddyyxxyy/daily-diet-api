05f8bb54ca0fff0aac07458511d1a0a390d9fcad
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-17 13:37:34 -0300

    fix: solves knexfile configs not being loaded by knex cli

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,10 @@
 import knex, { Knex } from 'knex';
 
-import { development, production } from '../../knexfile';
+import { development, production } from './config';
 import { env } from '../env';
 
 const connConfig = env.NODE_ENV === 'production' ? production : development;
 
 const conn: Knex = knex(connConfig);
 
-export { conn };
+export { conn, connConfig };
