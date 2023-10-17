b94cba5e104d2b22dfb379511a96cc50e938b789
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-17 18:40:50 -0300

    types: enhances knex tables autocomplete for insertions and updates

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,23 +2,38 @@
 import { Knex } from 'knex';
 
 declare module 'knex/types/tables' {
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+  }
+
+  interface Meal {
+    id: string;
+    name: string;
+    description: string;
+    date: string;
+    hour: string;
+    isOnTheDiet: boolean;
+    createdAt: string;
+    modifiedAt?: string;
+    userId: string;
+  }
+
   export interface Tables {
-    users: {
-      id: string;
-      name: string;
-      email: string;
-      password: string;
-    };
-    meals: {
-      id: string;
-      name: string;
-      description: string;
-      date: string;
-      hour: string;
-      isOnTheDiet: boolean;
-      createdAt: string;
-      modifiedAt?: string;
-      userId: string;
-    };
+    users: Knex.CompositeTableType<
+      User,
+      Pick<User, 'name' | 'email' | 'password'>,
+      Pick<User, 'name' | 'email' | 'password'>
+    >;
+    meals: Knex.CompositeTableType<
+      Meal,
+      Pick<Meal, 'name' | 'description' | 'date' | 'hour' | 'isOnTheDiet'>,
+      Pick<
+        Meal,
+        'name' | 'description' | 'date' | 'hour' | 'isOnTheDiet' | 'modifiedAt'
+      >
+    >;
   }
 }
