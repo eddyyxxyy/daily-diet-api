deeec2e149e81bfa95d03a2b01a972f8f63af8b2
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-10-24 20:56:18 -0300

    feat(config/jwt): :sparkles: deletes expired refresh tokens every hour

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import fastifyJwt from '@fastify/jwt';
 import fastify from 'fastify';
 import { FastifyInstance } from 'fastify/types/instance';
 
+import { conn } from './database';
 import { env } from './env';
 import { mealsRoutes } from './routes/meals.routes';
 import { sessionsRoutes } from './routes/sessions.routes';
@@ -24,3 +25,10 @@ app.register(sessionsRoutes, {
 app.register(mealsRoutes, {
   prefix: 'meals',
 });
+
+async function deleteExpiredTokens() {
+  const now = new Date();
+  await conn('tokens').delete().where('expiry', '<', now.toISOString());
+}
+
+setInterval(deleteExpiredTokens, 60 * 60 * 1000);
