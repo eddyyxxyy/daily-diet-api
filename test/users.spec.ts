62c1e497a369ac6c91cdb9ba783d6615e86ad1d8
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-11-01 11:05:26 -0300

    test(routes): :white_check_mark: tests /summary route

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -55,4 +55,102 @@ describe('users routes', () => {
       }),
     );
   });
+
+  it("should be able to get user's summary", async () => {
+    const userToBeCreated = {
+      name: 'Edson Pimenta',
+      email: 'test@email.com',
+      password: '123@Test',
+    };
+
+    await request(app.server)
+      .post('/users')
+      .send({ ...userToBeCreated });
+
+    const loginResponse = await request(app.server).post('/sessions').send({
+      email: userToBeCreated.email,
+      password: userToBeCreated.password,
+    });
+
+    let token = '';
+
+    loginResponse.get('Set-Cookie').forEach((cookie) => {
+      if (cookie.startsWith('@daily-diet:accessToken')) {
+        const notSanitizedToken = cookie.split(';')[0];
+        token = notSanitizedToken.split('=')[1];
+      }
+    });
+
+    for (let i = 1; i < 4; i++) {
+      await request(app.server)
+        .post('/meals')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          name: `Testing summary ${i}`,
+          description: 'Um monte de folha muito foda',
+          date: `2023-11-0${i}`,
+          hour: '20:00',
+          isOnTheDiet: true,
+        });
+    }
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Testing summary 4',
+        description: 'Um monte de folha muito foda',
+        date: '2023-11-04',
+        hour: '20:00',
+        isOnTheDiet: false,
+      });
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Testing summary 5',
+        description: 'Um monte de folha muito foda',
+        date: '2023-11-05',
+        hour: '20:00',
+        isOnTheDiet: true,
+      });
+
+    await request(app.server)
+      .post('/meals')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Testing summary 6',
+        description: 'Um monte de folha muito foda',
+        date: '2023-11-06',
+        hour: '20:00',
+        isOnTheDiet: false,
+      });
+
+    for (let i = 7; i < 13; i++) {
+      await request(app.server)
+        .post('/meals')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          name: `Testing summary ${i}`,
+          description: 'Um monte de folha muito foda',
+          date: `2023-11-0${i}`,
+          hour: '20:00',
+          isOnTheDiet: true,
+        });
+    }
+
+    const summaryResponse = await request(app.server)
+      .get('/users/summary')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(summaryResponse.body).toEqual({
+      summary: {
+        totalOfMeals: 12,
+        totalOfMealsOnDiet: 10,
+        totalOfMealsOutOfDiet: 2,
+        bestSequenceOfMealsOnDiet: 6,
+      },
+    });
+  });
 });
