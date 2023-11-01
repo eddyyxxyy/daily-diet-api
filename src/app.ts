340c14bfdf99a63db9cc7bfe8c067a0b894da294
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-11-01 12:28:21 -0300

    feat(sec): :sparkles: implements cors and security policies
    Uses @fastify/cors and @fastify/helmet with different settings for dev and prod

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import cookie from '@fastify/cookie';
+import fastifyCors from '@fastify/cors';
+import helmet from '@fastify/helmet';
 import fastifyJwt from '@fastify/jwt';
 import fastify from 'fastify';
 import { FastifyInstance } from 'fastify/types/instance';
@@ -12,6 +14,38 @@ import { usersRoutes } from './routes/users.routes';
 export const app: FastifyInstance = fastify();
 
 // Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.register(helmet, {
+    contentSecurityPolicy: false,
+  });
+} else {
+  app.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"],
+        objectSrc: ["'none'"],
+      },
+    },
+    hsts: {
+      maxAge: 15552000, // 180 days
+      includeSubDomains: true,
+      preload: true,
+    },
+    noSniff: true,
+    frameguard: {
+      action: 'deny',
+    },
+    referrerPolicy: {
+      policy: 'same-origin',
+    },
+  });
+}
+app.register(fastifyCors, {
+  origin: [`http://localhost:${env.PORT}`, env.FRONT_END_URL],
+  credentials: true,
+});
 app.register(cookie);
 app.register(fastifyJwt, { secret: env.JWT_SECRET });
 
